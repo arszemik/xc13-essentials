eed24b8811a2bae0e398c12138dd25490af42970
Damian Biniarz <arszemik@icloud.com>
2022-08-09 14:20:28 +0200

    Initial Commit

---
+++ SourceControlFromExisting/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SourceControlFromExisting
+//
+//  Created by Damian Biniarz on 2022-08-09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SourceControlFromExisting/SourceControlFromExistingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SourceControlFromExistingApp.swift
+//  SourceControlFromExisting
+//
+//  Created by Damian Biniarz on 2022-08-09.
+//
+
+import SwiftUI
+
+@main
+struct SourceControlFromExistingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
